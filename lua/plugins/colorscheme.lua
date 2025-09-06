eb81001e68ecdf479e5a2ccc84042d86d11a67a4
John Zambrano <johnnyz88@gmail.com>
2025-09-06 15:28:44 +1000

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,6 +22,7 @@ return {
     lazy = true,
     name = "catppuccin",
     opts = {
+      transparent_background = true,
       integrations = {
         alpha = true,
         cmp = true,
@@ -65,22 +66,22 @@ return {
         devicons = false, -- highlight the icons of `nvim-web-devicons`
         styles = {
           comment = { italic = true },
-          keyword = { italic = true }, -- any other keyword
-          type = { italic = true }, -- (preferred) int, long, char, etc
-          storageclass = { italic = true }, -- static, register, volatile, etc
-          structure = { italic = true }, -- struct, union, enum, etc
-          parameter = { italic = true }, -- parameter pass in function
+          keyword = { italic = true },       -- any other keyword
+          type = { italic = true },          -- (preferred) int, long, char, etc
+          storageclass = { italic = true },  -- static, register, volatile, etc
+          structure = { italic = true },     -- struct, union, enum, etc
+          parameter = { italic = true },     -- parameter pass in function
           annotation = { italic = true },
           tag_attribute = { italic = true }, -- attribute of tag in reactjs
         },
-        filter = "machine", -- classic | octagon | pro | machine | ristretto | spectrum
+        filter = "machine",                  -- classic | octagon | pro | machine | ristretto | spectrum
         -- Enable this will disable filter option
         day_night = {
-          enable = false, -- turn off by default
-          day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+          enable = false,            -- turn off by default
+          day_filter = "pro",        -- classic | octagon | pro | machine | ristretto | spectrum
           night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
         },
-        inc_search = "background", -- underline | background
+        inc_search = "background",   -- underline | background
         background_clear = {
           -- "float_win",
           "toggleterm",
